732b138de5884265b133d73a5a7178e33ba9f7f2
Mikebulatov <mikebulatov@yandex.ru>
2023-09-07 19:35:34 +0700

    Задача 62.

--- zd62/Program.cs
+++ zd62/Program.cs
@@ -1,6 +1,5 @@
 ﻿Console.WriteLine("Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.");
 
-
 int [,] matrixA=new int [4,4];
 //формируем матрицы
 int k=0;
@@ -25,8 +24,19 @@ for(int i=matrixA.GetLength(1)-1; i>0; i--)
     k=k+1;
     matrixA[i,0]=k;
 }
+for(int i=1; i<matrixA.GetLength(0)-1; i++)
+{
+    k=k+1;
+    matrixA[1,i]=k;
+}
+for(int i=matrixA.GetLength(0)-2; i>0; i--)
+{
+    k=k+1;
+    matrixA[2,i]=k;
+}
 
-Console.WriteLine("Сформированна матрица А:");
+
+Console.WriteLine("Сформированна матрица:");
 for(int i=0; i<matrixA.GetLength(0); i++)
 {
     for(int j=0; j<matrixA.GetLength(1); j++)
